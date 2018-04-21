0ec849143342a99e36f5168bd55b0472f3596aea
Jeka98 <Zhukjeka1998@yandex.by>
2018-04-21 22:20:39 +0300

    add some post

--- databaseCategory/app/routes/note_routes.js
+++ databaseCategory/app/routes/note_routes.js
@@ -3,8 +3,8 @@ module.exports = function(app, db) {
 
     //get category by id
     app.get('/notes/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
+        const id       = req.params.id;
+        const details  = { '_id': new ObjectID(id) };
         db.collection('notes').findOne(details, (err, item) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
@@ -36,8 +36,8 @@ module.exports = function(app, db) {
 
     //delete category by id
     app.delete('/notes/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
+        const id       = req.params.id;
+        const details  = { '_id': new ObjectID(id) };
         db.collection('notes').remove(details, (err, item) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
@@ -49,9 +49,9 @@ module.exports = function(app, db) {
 
     //update category notes from postman body
     app.put ('/notes/:id', (req, res) => {
-        const id = req.params.id;
-        const details = { '_id': new ObjectID(id) };
-        const note = { text: req.body.body, title: req.body.title };
+        const id       = req.params.id;
+        const details  = { '_id': new ObjectID(id) };
+        const note     = { text: req.body.body, title: req.body.title };
         db.collection('notes').update(details, note, (err, result) => {
             if (err) {
                 res.send({'error':'An error has occurred'});
@@ -61,7 +61,7 @@ module.exports = function(app, db) {
         });
     });
 
-    //create category with title from url
+    //create category with name from url
     app.post('/notes/add/:name', (req,res) => {
         const name = { title: req.params.name};
         db.collection('notes').insert(name, (err, result) => {
@@ -72,5 +72,64 @@ module.exports = function(app, db) {
             }
         });
     });
+
+
+    //localhost:8000/notes/5ada5cb72968c02524130713/phone/someURL/300
+    app.post('/notes/:id/:prodCatName/:prodName/:img/:price', (req, res) => {
+        const id           = req.params.id;
+        const prodID       = ObjectID();
+        const prodName     = req.params.prodName;
+        const img          = req.params.img;
+        const price        = req.params.price;
+        const details      = { '_id': new ObjectID(id) };
+        //const product    = {id:  prodID , name: req.params.prodName, img: req.params.img, price: req.params.price }
+        const takeCatName  =  req.params.prod;
+        const product      = {id:  prodID , name: req.params.prodName, img: req.params.img, price: req.params.price }
+        
+
+        db.collection('notes').findOne(details, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                //db.collection.update(  { _id:...} , { $set: { some_key : new_info  } }
+                db.collection('notes').update(details,{ $set :  {product}});
+            }
+            res.send(result);
+        });
+    });
+
+
+    /////&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7
+    app.post('/notes/:id/prodID/:prodID', (req,res) => {
+        const id        = req.params.id;
+        const prodID    = req.params.prodID;
+        const details   = { '_id': new ObjectID(id) };
+
+        db.collection('notes').findOne(details, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                db.collection('notes').update({details}, { $pull: { "product" : { "id" : prodID } } }, false, true);
+                res.send('Note ' + prodID + ' deleted!');
+            }
+        });
+    });
+
+    //  UserModel.findById(id, function (err, user) { ... } );
+
+    //User.find({ _id: '55822f34a8394683dd015888' });  
+
+    app.get('/notes/getCatById/:id',(req,res) => {
+        const id        = req.params.id;
+        const details   = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne (details, (err, result) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                res.send(result);
+            }
+        });
+    });
+
 
 };
