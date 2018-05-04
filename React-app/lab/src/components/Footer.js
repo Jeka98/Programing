e70c2f4b57ae7461df584d343281e88f2aa95657
Jeka98 <Zhukjeka1998@yandex.by>
2018-05-05 00:32:40 +0300

    footer fix

---
+++ React-app/lab/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../css/style.css';
+
+class Footer extends Component {
+    render() {
+      return (
+        <footer className="fixed-bottom footer-copyright py-3 text-center">© 2018 Copyright</footer>
+      );
+    }
+  }
+
+export default Footer;
